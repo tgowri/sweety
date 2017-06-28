295b7001d37da76803539344911c4ed7a3c6da5e
tgowri <gowrithulasiraj@gmail.com>
2017-06-28 14:23:24 +0530

    Create pink.c

---
+++ pink.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value of a:");
+scanf("%d",&a);
+if(a>0)
+printf("\n positive number");
+if(a<0)
+printf("\n negative number");
+if(a=0)
+printf("\n zero");
+getch();
+}
